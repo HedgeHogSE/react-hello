4f97dcca0cd942e32a0667d63e8f118aaca13e98
HedgeHogSE <sasha.ezhak2006@gmail.com>
2026-03-25 10:54:47 +0400

    delete bugs 2

--- react-hello/src/App.js
+++ react-hello/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import HelloPage from "./components/pages/HelloPage";
 import WorldPage from "./components/pages/WorldPage";
 import './App.css';
