2ba58361cc8eb7bd665c219e849d59e0f9140160
HedgeHogSE <sasha.ezhak2006@gmail.com>
2026-03-25 10:51:01 +0400

    delete bugs

--- react-hello/src/App.js
+++ react-hello/src/App.js
@@ -6,7 +6,6 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <Header />
       <Routes>
         <Route path="/hello" element={<HelloPage />} />
         <Route path="/world" element={<WorldPage />} />
